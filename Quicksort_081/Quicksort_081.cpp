12ee1f8c05f6bad647f1cda7b7b198d4d655b96e
namira121 <127211008+namira121@users.noreply.github.com>
2023-03-31 16:02:13 +0700

    menyusun list di sebelah kiri dengan quicksort

--- Quicksort_081/Quicksort_081.cpp
+++ Quicksort_081/Quicksort_081.cpp
@@ -77,7 +77,7 @@ void q_short(int low, int high) {
 		mov_count++; 
 	}
 	//sort the list on the left of pivot using quick sort 
-	q_short(low, j - 1);//langkah 12
+	q_short(low, j - 1);//langkah 12 
 
 	//sort the list on the right of pivot using quick sort
 	q_short(j + 1, high);//langkah 13
